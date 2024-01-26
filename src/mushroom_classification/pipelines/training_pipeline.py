065dd094eb0d28a3815cec638465b27ea68b846d
Annamalai-Subramani <annamalai008s@gmail.com>
2024-01-26 15:55:57 +0530

    model evaluation added

--- src/mushroom_classification/pipelines/training_pipeline.py
+++ src/mushroom_classification/pipelines/training_pipeline.py
@@ -7,7 +7,7 @@ import numpy as np
 from mushroom_classification.components.data_ingestion import DataIngestion
 from mushroom_classification.components.data_transformation import DataTransformation
 from mushroom_classification.components.model_trainer import ModelTrainer
-
+from mushroom_classification.components.model_evaluation import ModelEvaluation
 
 logging.info('Training Pipeline has started')
 
@@ -21,6 +21,8 @@ try:
     model_trainer_obj=ModelTrainer()
     model_trainer_obj.initate_model_training(train_arr,test_arr)
 
+    model_eval_obj = ModelEvaluation()
+    model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
 
 
 except Exception as e:
